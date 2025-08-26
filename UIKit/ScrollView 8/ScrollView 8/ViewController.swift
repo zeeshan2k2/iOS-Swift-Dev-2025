2cc94527c5a53bc7a5db5c457ad957a171b78a2c
zeeshan2k2 <zeeshan.12.zw@gmail.com>
2025-08-26 19:04:19 +0500

    scroll view added

---
+++ UIKit/ScrollView 8/ScrollView 8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollView 8
+//
+//  Created by Zeeshan Waheed on 26/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
