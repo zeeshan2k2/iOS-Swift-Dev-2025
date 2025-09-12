774b5008effef0d5c665a0e1e9add81556187cbe
zeeshan2k2 <zeeshan.12.zw@gmail.com>
2025-09-12 19:02:04 +0500

    typography 2

---
+++ UIKit/Type 15/Type 15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Type 15
+//
+//  Created by Zeeshan Waheed on 12/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIKit/Type 15/Type 15UITests/Type_15UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Type_15UITestsLaunchTests.swift
+//  Type 15UITests
+//
+//  Created by Zeeshan Waheed on 12/09/2025.
+//
+
+import XCTest
+
+final class Type_15UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
