7937a2c821f062a398552869a9b0cb9c4ad6ef46
zeeshan2k2 <zeeshan.12.zw@gmail.com>
2025-09-29 20:04:55 +0500

    colors

---
+++ UIKit/Color 17/Color 17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color 17
+//
+//  Created by Zeeshan Waheed on 19/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myBlueColor = UIColor(red: 142/255, green: 187/255, blue: 255/255, alpha: 1)
+    }
+
+
+}
+
---
+++ UIKit/Color 17/Color 17UITests/Color_17UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Color_17UITestsLaunchTests.swift
+//  Color 17UITests
+//
+//  Created by Zeeshan Waheed on 19/09/2025.
+//
+
+import XCTest
+
+final class Color_17UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
