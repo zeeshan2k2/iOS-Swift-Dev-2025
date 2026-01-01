086520e39e0141a368e1ae2692fa42a00a09c72d
zeeshan2k2 <zeeshan.12.zw@gmail.com>
2026-01-02 00:18:16 +0500

    animationadded

--- UIKit/ToDoList 12/ToDoList 12/NewTaskViewController.swift
+++ UIKit/ToDoList 12/ToDoList 12/NewTaskViewController.swift
@@ -38,10 +38,26 @@ class NewTaskViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black.withAlphaComponent(0.9)
-        
+        modalView.transform = CGAffineTransform(scaleX: 0, y: 0)
         view.addSubview(modalView)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // spring with damping value less means more bouncing and vice versa
+        UIView.animate(withDuration: 0.35, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 5, options: [.curveEaseOut]) {
+            self.modalView.transform = CGAffineTransform.identity
+        }
+        
+//        UIView.animate(withDuration: 0.25, delay: 0, options: [.curveEaseOut]) {
+//            self.modalView.transform = CGAffineTransform.identity
+//        }
+        
+//        UIView.animate(withDuration: 0.25) {
+//            self.modalView.transform = CGAffineTransform.identity
+//        }
+    }
 }
 
 
